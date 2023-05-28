393d6fb46bd3bb3171ad146bf638ee2fac5fa9e8
Gagik Kazandjyan <bi0820a@outlook.com>
2023-05-28 22:26:48 +0400

    Have to complete this task

--- src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
+++ src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
@@ -2,67 +2,66 @@ package com.epam.rd.autotasks.figures;
 
 import java.util.Arrays;
 
-class Quadrilateral extends Figure {
-    private final Point p1;
-    private final Point p2;
-    private final Point p3;
-    private final Point p4;
+public class Quadrilateral extends Figure {
+    private final Point[] vertices;
 
-    public Quadrilateral(Point p1, Point p2, Point p3, Point p4) {
-        validateQuadrilateral(p1, p2, p3, p4);
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.p4 = p4;
+    public Quadrilateral(Point vertex1, Point vertex2, Point vertex3, Point vertex4) {
+        this.vertices = new Point[]{vertex1, vertex2, vertex3, vertex4};
+        validateQuadrilateral();
+    }
+
+    private void validateQuadrilateral() {
+        if (vertices[0].equals(vertices[1]) || vertices[0].equals(vertices[2]) || vertices[0].equals(vertices[3])
+                || vertices[1].equals(vertices[2]) || vertices[1].equals(vertices[3]) || vertices[2].equals(vertices[3])) {
+            throw new IllegalArgumentException("Quadrilateral is degenerative");
+        }
+
+        if (!isConvex(vertices)) {
+            throw new IllegalArgumentException("Quadrilateral is not convex");
+        }
+    }
+
+    private boolean isConvex(Point[] vertices) {
+        int orientationABD = getOrientation(vertices[0], vertices[1], vertices[3]);
+        int orientationABC = getOrientation(vertices[0], vertices[1], vertices[2]);
+        int orientationBCD = getOrientation(vertices[1], vertices[2], vertices[3]);
+        int orientationCDA = getOrientation(vertices[2], vertices[3], vertices[0]);
+
+        return orientationABD == orientationABC && orientationABC == orientationBCD && orientationBCD == orientationCDA;
+    }
+
+    private int getOrientation(Point p, Point q, Point r) {
+        double value = (q.getY() - p.getY()) * (r.getX() - q.getX())
+                - (q.getX() - p.getX()) * (r.getY() - q.getY());
+
+        if (value == 0) {
+            return 0; // Collinear
+        } else if (value > 0) {
+            return 1; // Clockwise orientation
+        } else {
+            return -1; // Counterclockwise orientation
+        }
     }
 
     @Override
     public Point centroid() {
-        double cx = (p1.getX() + p2.getX() + p3.getX() + p4.getX()) / 4.0;
-        double cy = (p1.getY() + p2.getY() + p3.getY() + p4.getY()) / 4.0;
-        return new Point(cx, cy);
+        double centerX = (vertices[0].getX() + vertices[1].getX() + vertices[2].getX() + vertices[3].getX()) / 4;
+        double centerY = (vertices[0].getY() + vertices[1].getY() + vertices[2].getY() + vertices[3].getY()) / 4;
+        return new Point(centerX, centerY);
     }
 
     @Override
     public boolean isTheSame(Figure figure) {
         if (figure instanceof Quadrilateral) {
             Quadrilateral other = (Quadrilateral) figure;
-            Point[] otherVertices = {other.p1, other.p2, other.p3, other.p4};
-            return Arrays.asList(otherVertices).containsAll(Arrays.asList(p1, p2, p3, p4)) &&
-                    Arrays.asList(p1, p2, p3, p4).containsAll(Arrays.asList(otherVertices));
+            return Arrays.equals(this.vertices, other.vertices);
         }
         return false;
     }
-
-    private void validateQuadrilateral(Point p1, Point p2, Point p3, Point p4) {
-        if (p1 == null || p2 == null || p3 == null || p4 == null) {
-            throw new IllegalArgumentException("Points cannot be null");
-        }
-
-        double area1 = 0.5 * (p1.getX() * (p2.getY() - p4.getY())
-                + p2.getX() * (p4.getY() - p1.getY())
-                + p4.getX() * (p1.getY() - p2.getY()));
-
-        double area2 = 0.5 * (p2.getX() * (p3.getY() - p1.getY())
-                + p3.getX() * (p1.getY() - p2.getY())
-                + p1.getX() * (p2.getY() - p3.getY()));
-
-        if (area1 == 0 || area2 == 0) {
-            throw new IllegalArgumentException("Quadrilateral is degenerate");
-        }
-
-        double areaSum = Math.abs(area1) + Math.abs(area2);
-        if (Math.abs(areaSum) <= 0.000001) {
-            throw new IllegalArgumentException("Quadrilateral is degenerate");
-        }
-
-        double crossProduct1 = (p2.getX() - p1.getX()) * (p4.getY() - p3.getY()) - (p2.getY() - p1.getY()) * (p4.getX() - p3.getX());
-        double crossProduct2 = (p3.getX() - p2.getX()) * (p1.getY() - p4.getY()) - (p3.getY() - p2.getY()) * (p1.getX() - p4.getX());
-        if (crossProduct1 * crossProduct2 >= 0) {
-            throw new IllegalArgumentException("Quadrilateral is not convex");
-        }
-    }
 }
+
+
+
 
 
 
